537b3574629f94becc9f1d5768dd37ef0f08f17f
Abracadabra <53264371+ShimizuYukii@users.noreply.github.com>
2024-10-21 19:19:47 -0400

    Add files via upload

---
+++ RS_data/Record Linkage.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 11:28:52 2024
+
+@author: Kewei Xu
+"""
+
+
+import recordlinkage
+from recordlinkage.datasets import load_febrl4
+
+dfA, dfB = load_febrl4()
+dfA
+
+
+
+
